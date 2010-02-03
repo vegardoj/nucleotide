ffb605dd85d18679e98d1bfc2a8ecdbfda8b484c
martin <martin@6ebd7ac6-aa6e-4c2c-8376-f0191a6cfc88>
2010-02-03 20:13:26 +0000

    Cleaned up markup Added prettier design Added a sample dojo chart
    git-svn-id: file:///home/martin/svn/repositories/nucleotide@4 6ebd7ac6-aa6e-4c2c-8376-f0191a6cfc88

--- trunk/public/javascripts/app.js
+++ trunk/public/javascripts/app.js
@@ -2,17 +2,28 @@ dojo.require("dijit.form.Button");
 dojo.require("dijit.form.TextBox");
 dojo.require("dijit.form.Form");
 dojo.require("dijit.form.ValidationTextBox");
+dojo.require("dojox.charting.Chart2D");
+
+function makeChart() {
+    var chart = new dojox.charting.Chart2D("result");
+    chart.addPlot("default", {type: "Lines", markers: true,
+        tension:3, shadows: {dx: 2, dy: 2, dw: 2}});
+    chart.addAxis("x");
+    chart.addAxis("y", {vertical: true});
+    chart.addSeries("Series 1", [1, 2, 2, 3, 4, 5, 5, 7]);
+    chart.render();
+}
 
 function sendForm() {
-    var button = dijit.byId("analyze");
+    var button = dijit.byId("button");
     dojo.connect(button, "onClick", function(event) {
         event.preventDefault();
         event.stopPropagation();
         var xhrArgs = {
-            form: dojo.byId("analyze_form"),
+            form: dojo.byId("input"),
             handleAs: "text",
             load: function(data) {
-                dojo.byId("result").innerHTML = data;
+                makeChart();
             },
             error: function(error) {
                 dojo.byId("result").innerHTML = i18n('error');
@@ -21,4 +32,5 @@ function sendForm() {
         var deferred = dojo.xhrPost(xhrArgs);
     });
 }
+
 dojo.addOnLoad(sendForm);

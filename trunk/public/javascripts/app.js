9b567021ea342ef0eaa389f6c722ebd20976678a
martin <martin@6ebd7ac6-aa6e-4c2c-8376-f0191a6cfc88>
2010-03-08 12:57:47 +0000

    Minor fixes
    git-svn-id: file:///home/martin/svn/repositories/nucleotide@7 6ebd7ac6-aa6e-4c2c-8376-f0191a6cfc88

--- trunk/public/javascripts/app.js
+++ trunk/public/javascripts/app.js
@@ -7,20 +7,20 @@ dojo.require("dojox.charting.Chart2D");
 
 var chartBuilder = (function() {
     var chart = null;
+    var makeChart = function() {
+        if (chart !== null) {
+            console.log("chart exists, destroying");
+            chart.destroy();
+        }
+        chart = new dojox.charting.Chart2D("result");
+        chart.addPlot("default", {type: "Lines", markers: true,
+            tension:3, shadows: {dx: 2, dy: 2, dw: 2}});
+        chart.addAxis("x");
+        chart.addAxis("y", {vertical: true});
+        chart.addSeries("Series 1", [1, 2, 2, 3, 4, 5, 5, 7]);
+        chart.render();
+    };
     return {
-        makeChart: function() {
-            if (chart !== null) {
-                console.log("chart exists, destroying");
-                chart.destroy();
-            }
-            chart = new dojox.charting.Chart2D("result");
-            chart.addPlot("default", {type: "Lines", markers: true,
-                tension:3, shadows: {dx: 2, dy: 2, dw: 2}});
-            chart.addAxis("x");
-            chart.addAxis("y", {vertical: true});
-            chart.addSeries("Series 1", [1, 2, 2, 3, 4, 5, 5, 7]);
-            chart.render();
-        },
         sendForm: function() {
             var button = dijit.byId("button");
             dojo.connect(button, "onClick", function(event) {
@@ -30,7 +30,7 @@ var chartBuilder = (function() {
                     form: dojo.byId("input"),
                     handleAs: "text",
                     load: function(data) {
-                        chartBuilder.makeChart();
+                        makeChart();
                     },
                     error: function(error) {
                         dojo.byId("result").innerHTML = i18n('error');

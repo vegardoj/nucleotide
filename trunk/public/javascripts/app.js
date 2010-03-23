7f40e78968e8975bd9968bc45da393c29250df7d
martin <martin@6ebd7ac6-aa6e-4c2c-8376-f0191a6cfc88>
2010-03-23 15:33:57 +0000

    - Added dialog for viewing resulting graph - Added dependencies for Grafico js lib
    git-svn-id: file:///home/martin/svn/repositories/nucleotide@8 6ebd7ac6-aa6e-4c2c-8376-f0191a6cfc88

--- trunk/public/javascripts/app.js
+++ trunk/public/javascripts/app.js
@@ -4,17 +4,18 @@ dojo.require("dijit.form.TextBox");
 dojo.require("dijit.form.Form");
 dojo.require("dijit.form.ValidationTextBox");
 dojo.require("dojox.charting.Chart2D");
+dojo.require("dijit.Dialog");
 
 var chartBuilder = (function() {
     var chart = null;
     var makeChart = function() {
         if (chart !== null) {
-            console.log("chart exists, destroying");
+            //console.log("chart exists, destroying");
             chart.destroy();
         }
         chart = new dojox.charting.Chart2D("result");
-        chart.addPlot("default", {type: "Lines", markers: true,
-            tension:3, shadows: {dx: 2, dy: 2, dw: 2}});
+        chart.addPlot("default", {type: "Lines", markers: false,
+            tension:3, shadows: {dx: 20, dy: 13, dw: 2}});
         chart.addAxis("x");
         chart.addAxis("y", {vertical: true});
         chart.addSeries("Series 1", [1, 2, 2, 3, 4, 5, 5, 7]);
@@ -22,7 +23,7 @@ var chartBuilder = (function() {
     };
     return {
         sendForm: function() {
-            var button = dijit.byId("button");
+            var button = dijit.byId("calculate");
             dojo.connect(button, "onClick", function(event) {
                 event.preventDefault();
                 event.stopPropagation();
@@ -38,7 +39,12 @@ var chartBuilder = (function() {
                 }
                 var deferred = dojo.xhrPost(xhrArgs);
             });
+        },
+        createDialog: function() {
+            var color = dijit.byId("dialogColor");
+            dojo.connect(dijit.byId("calculate"), "onClick", color, "show");
         }
     }
 })();
 dojo.addOnLoad(chartBuilder.sendForm);
+dojo.addOnLoad(chartBuilder.createDialog);
